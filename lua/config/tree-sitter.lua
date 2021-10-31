935c589466a50f19801501b8eb0128f71c37fbfe
xiuxiu62 <jacremer@live.com>
2021-10-31 15:08:47 -0700

    update treesitter config

--- lua/config/tree-sitter.lua
+++ lua/config/tree-sitter.lua
@@ -1,20 +1,25 @@
-require('nvim-treesitter')
-
 require'nvim-treesitter.configs'.setup {
-    ts_funky_keywords = {enable = true},
-    highlight = {
-        enable = true,
-        custom_captures = {}
-    },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25 -- Debounced time for highlighting nodes in the playground from source code
-    },
-    query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = {"BufWrite", "CursorHold"}
-    },
-    context_commentstring = {enable = true}
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = false,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "html",
+    "scss"
+  },
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.used_by = { "javascript", "typescript.tsx" }
+
